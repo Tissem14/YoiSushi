19037811bbc0b345994ef13b7a41ab57e3488937
Tissem14 <tissem.idiripro@gmail.com>
2024-06-13 06:19:54 +0200

    Création du Panier et de notre classe Cart, Création de la vue récapitulatif panier, Création des fonctions permettant Ajout/diminution/suppression de produit dans le panier, Création de l'entity Address

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class AccountAddressController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/compte/address', name: 'account_address')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('account/address.html.twig', []);
     }
 }
